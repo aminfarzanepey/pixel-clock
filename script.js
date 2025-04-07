eec1f7f999d6d762c6749602dbfa2b4aeed449c2
Amin Farzanepey <aminfarzanepey@gmail.com>
2025-04-07 15:31:39 +0330

    add pixel font and add theme and format future

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const formatBtn = document.getElementById("format-btn");
+const themeBtn = document.getElementById("theme-btn");
+let is24Hour = true;
+
 function updateClock() {
     const now = new Date();
 
@@ -5,14 +9,28 @@ function updateClock() {
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
 
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+    if(!is24Hour){
+        hours = hours % 12 || 12;
+    }
 
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementById("clock").textContent = timeString;
+
+    const formatted = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+    document.getElementById("clock").textContent = formatted;
+}
+
+function pad(num) {
+    return num.toString().padStart(2, '0');
 }
 
 setInterval(updateClock,1000);
 
 updateClock();
+
+formatBtn.addEventListener("click", ()=>{
+    is24Hour = !is24Hour;
+    updateClock();
+})
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+})
